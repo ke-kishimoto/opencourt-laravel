03051733ae57d79b1cb966976dcaa350bff5fd38
ke-kishimoto <nebinosuk@gmail.com>
2021-10-30 22:16:08 +0900

    データ追加

--- database/seeders/MemberCategorySeeder.php
+++ database/seeders/MemberCategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MemberCategorySeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class MemberCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('member_categories')->insert([
+            [
+                "category_name" => "社会人"
+            ]
+        ]);
+        DB::table('member_categories')->insert([
+            [
+                "category_name" => "大学生"
+            ]
+        ]);
+        DB::table('member_categories')->insert([
+            [
+                "category_name" => "高校生"
+            ]
+        ]);
     }
 }
