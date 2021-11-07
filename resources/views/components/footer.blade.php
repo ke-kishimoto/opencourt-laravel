aef1befea923412f56ab0598dc2f3b2ec70675ad
ke-kishimoto <nebinosuk@gmail.com>
2021-11-07 17:06:11 +0900

    プライバシーポリシー

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div>
     <hr>
         <footer class="">
-            <a href="privacyPolicy/">プライバシーポリシー</a>
+            <a href="privacyPolicy">プライバシーポリシー</a>
             <a href="inquiry">お問い合わせ</a>
             <a href="notice">お知らせ</a>
         </footer>
