1f694460e52839f9250f35fe959274bc87f79f1e
ke-kishimoto <nebinosuk@gmail.com>
2022-08-14 10:03:07 +0900

    テンプレートの更新処理追加

--- app/Http/Controllers/EventTemplateController2.php
+++ app/Http/Controllers/EventTemplateController2.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\EventTemplate;
-use Illuminate\Support\Facades\Log;
 
 class EventTemplateController extends Controller
 {
+    public function index()
+    {
+        return view('eventTemplate', ['title' => 'イベントテンプレート']);
+    }
+
     public function getList()
     {
         return response(EventTemplate::select('id', 'id as value', 'template_name as name')->get(), 200);
@@ -27,24 +31,23 @@ class EventTemplateController extends Controller
 
     public function create(Request $request)
     {
-        Log::debug($request);
-        if($request->isNew) {
+        if($request->isnew) {
             $eventTemplate = new EventTemplate();
         } else {
-            $eventTemplate = EventTemplate::find($request->id);
+            $eventTemplate = EventTemplate::find($request->select_id);
         }
         $eventTemplate->template_name = $request->template_name;
         $eventTemplate->title = $request->title;
         $eventTemplate->short_title = $request->short_title;
         $eventTemplate->place = $request->place;
         $eventTemplate->limit_number = $request->limit_number;
-        $eventTemplate->description = $request->description;
+        $eventTemplate->detail = $request->detail;
         $eventTemplate->price1 = $request->price1;
         $eventTemplate->price2 = $request->price2;
         $eventTemplate->price3 = $request->price3;
         $eventTemplate->price4 = $request->price4;
         $eventTemplate->price5 = $request->price5;
         $eventTemplate->save();
-        return response($eventTemplate, 200);
+        return redirect()->route('eventTemplate');
     }
 }
