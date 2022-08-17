441fabe3f4e53e60b372c210c503ab80840e4c95
ke-kishimoto <nebinosuk@gmail.com>
2022-08-17 20:55:39 +0900

    コンポーネントも削除

--- resources/views/components/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <hr>
-        <footer class="">
-            <a href="privacyPolicy">プライバシーポリシー</a>
-            <a href="inquiry">お問い合わせ</a>
-            <a href="notice">お知らせ</a>
-        </footer>
-</div>
