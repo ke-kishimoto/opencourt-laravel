19847dcb29ce48bdc75aaa618536ddf64c541fa3
ke-kishimoto <nebinosuk@gmail.com>
2022-08-18 22:25:31 +0900

    notice → newsに変更

--- database/migrations/2021_10_30_160844_create_news_table.php
+++ database/migrations/2021_10_30_160844_create_news_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNoticesTable extends Migration
+class CreateNewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNoticesTable extends Migration
      */
     public function up()
     {
-        Schema::create('notices', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
             $table->string('content');
@@ -29,6 +29,6 @@ class CreateNoticesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notices');
+        Schema::dropIfExists('news');
     }
 }
