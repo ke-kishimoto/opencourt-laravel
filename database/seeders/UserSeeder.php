02abbc9df0aeac27615c0ce113b82ab13b923d39
ke-kishimoto <nebinosuk@gmail.com>
2022-08-18 22:23:04 +0900

    テストデータ調整

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,58 +16,40 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            "role_level" => 1,
+            "name" => "super_user",
+            "role_level" => 'system_admin',
             "email" => "super@test.com",
-            "name" => "super",
             "password" => Hash::make("password"),
             "user_category_id" => 1,
-            "gender" => 1,
-            "status" => 1,
+            "gender" => 'men',
+            "status" => 'active',
         ]);
         DB::table('users')->insert([
-            "role_level" => 1,
+            "role_level" => 'general',
             "email" => "nebinosuk@gmail.com",
             "name" => "kishimoto keisuke",
             "password" => Hash::make("password"),
             "user_category_id" => 1,
-            "gender" => 1,
-            "status" => 1,
+            "gender" => 'men',
+            "status" => 'active',
         ]);
         DB::table('users')->insert([
-            "role_level" => 1,
+            "role_level" => 'general',
             "email" => "k.kishimoto@tc-tech.co.jp",
             "name" => "tc-kishimoto",
             "password" => Hash::make("password"),
             "user_category_id" => 1,
-            "gender" => 1,
-            "status" => 1,
+            "gender" => 'men',
+            "status" => 'active',
         ]);
         DB::table('users')->insert([
-            "role_level" => 2,
+            "name" => "event_user",
+            "role_level" => 'event_admin',
             "email" => "admin@admin.com",
-            "name" => "admin",
             "password" => Hash::make("password"),
             "user_category_id" => 1,
-            "gender" => 1,
-            "status" => 1,
-        ]);
-        DB::table('users')->insert([
-            "role_level" => 3,
-            "email" => "test1@test.com",
-            "name" => "test1",
-            "password" => Hash::make("password"),
-            "user_category_id" => 1,
-            "gender" => 1,
-            "status" => 1,
-        ]);
-        DB::table('users')->insert([
-            "role_level" => 3,
-            "email" => "test2@test.com",
-            "name" => "test2",
-            "password" => Hash::make("password"),
-            "user_category_id" => 1,
-            "gender" => 1,
-            "status" => 1,
+            "gender" => 'men',
+            "status" => 'active',
         ]);
     }
 }
