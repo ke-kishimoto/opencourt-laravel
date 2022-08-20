39a1d98b1706e571dcd8a4462b01670f301ba61c
ke-kishimoto <nebinosuk@gmail.com>
2022-08-20 13:19:20 +0900

    不要な処理を削除

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginLogoutController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ConfigController;
-use App\Http\Controllers\EventTemplateController;
-use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,41 +13,6 @@ use App\Http\Controllers\EventController;
 |
 */
 
-// Route::middleware('auth:sanctum')->group(function(){ 
-//     Route::get('/index', function () {
-//         return view('index', ['title' => 'カレンダー']);
-//     });
-// });
-
 Route::get('/', function () {
-    return view('index', ['title' => 'カレンダー']);
-})->name('index');
-
-Route::get('/login', [LoginLogoutController::class, 'login']);
-Route::get('/logout', [LoginLogoutController::class, 'logout']);
-Route::post('/loginCheck', [LoginLogoutController::class, 'loginCheck']);
-Route::get('/newAccount', [UserController::class, 'newAccount']);
-Route::get('/privacyPolicy', function() {
-    return view('privacyPolicy', ['title' => 'プライバシーポリシー']);
+    return view('welcome');
 });
-Route::get('/config/{id}', [ConfigController::class, 'show']);
-Route::patch('/config/{id}/update', [ConfigController::class, 'update'])
-    ->name('config.update');
-Route::get('/eventTemplate', [EventTemplateController::class, 'index'])
-    ->name('eventTemplate');
-Route::post('/eventTemplate/create', [EventTemplateController::class, 'create'])
-    ->name('eventTemplate.create');
-Route::get('/userList', [UserController::class, 'list']);
-Route::get('/userDetail/{id}', [UserController::class, 'show']);
-Route::get('/userRegist', [UserController::class, 'regist']);
-Route::post('/user/create', [UserController::class, 'create'])
-    ->name('user.create');
-Route::get('/eventList', [EventController::class, 'list']);
-Route::get('/eventRegist', [EventController::class, 'regist']);
-Route::get('/eventDetail/{id}', [EventController::class, 'show']);
-Route::post('/event/create', [EventController::class, 'create'])
-    ->name('event.create');
-Route::post('/event/{id}/update', [EventController::class, 'update'])
-    ->name('event.update');
-Route::post('/event/{id}/delte', [EventController::class, 'delete'])
-    ->name('event.delete');
