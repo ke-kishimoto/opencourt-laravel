0c9355137cc1e55868f8d5c9bb3b0718eba9d276
ke-kishimoto <nebinosuk@gmail.com>
2022-08-24 22:37:01 +0900

    updateと前取得追加

--- app/Http/Controllers/EventTemplateController.php
+++ app/Http/Controllers/EventTemplateController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Log;
 
 class EventTemplateController extends Controller
 {
+    public function all()
+    {
+        return response(EventTemplate::all(), 200);
+    }
+
     public function getList()
     {
         return response(EventTemplate::select('id', 'id as value', 'template_name as name')->get(), 200);
@@ -27,12 +32,26 @@ class EventTemplateController extends Controller
 
     public function create(Request $request)
     {
-        Log::debug($request);
-        if($request->isNew || !$request->has('id')) {
-            $eventTemplate = new EventTemplate();
-        } else {
-            $eventTemplate = EventTemplate::find($request->id);
-        }
+        $eventTemplate = EventTemplate::create([
+          'template_name' => $request->template_name,
+          'title' => $request->title,
+          'short_title' => $request->short_title,
+          'place' => $request->place,
+          'limit_number' => $request->limit_number,
+          'description' => $request->description,
+          'price1' => $request->price1,
+          'price2' => $request->price1,
+          'price3' => $request->price2,
+          'price4' => $request->price3,
+          'price5' => $request->price4,
+        ]);
+
+        return response($eventTemplate, 200);
+    }
+
+    public function update(Request $request)
+    {
+        $eventTemplate = EventTemplate::find($request->id);
         $eventTemplate->template_name = $request->template_name;
         $eventTemplate->title = $request->title;
         $eventTemplate->short_title = $request->short_title;
@@ -45,6 +64,7 @@ class EventTemplateController extends Controller
         $eventTemplate->price4 = $request->price4;
         $eventTemplate->price5 = $request->price5;
         $eventTemplate->save();
+      
         return response($eventTemplate, 200);
     }
 }
