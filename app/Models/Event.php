6507771601c1d7e587089eccc96ede4d6a51fd32
ke-kishimoto <nebinosuk@gmail.com>
2022-09-03 23:37:08 +0900

    ステータス関連の処理追加

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Models\EventUser;
+use App\Models\Config;
 
 class Event extends Model
 {
@@ -25,7 +29,34 @@ class Event extends Model
 
     public function getStatusAttribute()
     {
-      return 1;
+      // 終了済み
+      if( $this->attributes['event_date'] < date("Y-m-d") ) {
+        return 'end';
+      }
+      // 参加登録済み
+      $user = app()->request->user();
+      if($user) {
+        $eventUser = EventUser::where('event_id', $this->attributes['id'])
+        ->where('user_id', $user->id)->first();
+        if($eventUser) {
+          return $eventUser->status === 'participation' ? 'participanted' : 'participanted_waiting';
+        }
+      }
+      // キャンセル待ち
+      $eventUser = EventUser::select(DB::raw('count(*) as cnt'))
+        ->where('event_id', $this->attributes['id'])->first();
+      $count = !$eventUser ? 0 : $eventUser->cnt;
+      if($count >= $this->attributes['limit_number']) {
+        return 'waiting';
+      }
+      // 残り僅か
+      $config = Config::find(1);
+      $borderNum = (!$config) ? 5 : $config->participant_border_number;
+      if($this->attributes['limit_number'] - $count <= $borderNum) {
+        return 'few';
+      }
+      // 空きあり
+      return 'afford';
     }
 
     public function eventUsers()
