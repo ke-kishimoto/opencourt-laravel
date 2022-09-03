00925a3ee7895a4404dbbf19f069ffb5fac54b67
ke-kishimoto <nebinosuk@gmail.com>
2022-09-03 23:37:29 +0900

    マイグレーション追加

--- database/migrations/2022_09_03_181016_create_policies_table.php
+++ database/migrations/2022_09_03_181016_create_policies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateConfigsTable extends Migration
+class CreatePoliciesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateConfigsTable extends Migration
      */
     public function up()
     {
-        Schema::create('configs', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('line_notify_flag');
-            $table->string('waiting_status_auto_update_flag');
-            $table->integer('participant_border_number');
+        Schema::create('policies', function (Blueprint $table) {
+            $table->id();
+            $table->string('policy_type');
+            $table->string('content');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -30,6 +28,6 @@ class CreateConfigsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('configs');
+        Schema::dropIfExists('policies');
     }
 }
--- database/migrations/2022_09_03_181155_create_event_view_view.php
+++ database/migrations/2022_09_03_181155_create_event_view_view.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateConfigsTable extends Migration
+class CreateEventViewView extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateConfigsTable extends Migration
      */
     public function up()
     {
-        Schema::create('configs', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('line_notify_flag');
-            $table->string('waiting_status_auto_update_flag');
-            $table->integer('participant_border_number');
+        Schema::create('event_view', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -30,6 +26,6 @@ class CreateConfigsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('configs');
+        Schema::dropIfExists('event_view');
     }
 }
